9ab7edca8103e08d1d56100d7f9f2b4eaa294d7f
Jester Rey Perez <165446384+jesterreyperez@users.noreply.github.com>
2026-09-05 07:37:48 +0800

    Start up five variables with a string value

---
+++ script.js
@@ -0,0 +1,7 @@
+const fortune1 =
+  "You will find money in your pocket today... but it will only be 5 pesos.";
+const fortune2 = "A mysterious stranger will compliment your code.";
+const fortune3 =
+  "Great success awaits you... right after you finish that JavaScript lesson.";
+const fortune4 = "You will become rich someday.";
+const fortune5 = "Beware! A bug is hiding in your code.";
