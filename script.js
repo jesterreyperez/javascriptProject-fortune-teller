6952f1620138d6774450dec69ad5d35b680b2325
Jester Rey Perez <165446384+jesterreyperez@users.noreply.github.com>
2026-09-21 12:02:45 +0800

    Replace if-else with switch statement

--- script.js
+++ script.js
@@ -9,20 +9,26 @@ const fortune5 = "Beware! A bug is hiding in your code.";
 const min = 1;
 const max = 5;
 
-const randomNumber = Math.floor(Math.random() * (max - min + 1) + min); //Logic to generate a random number between 1 and 5
+const randomNumber = Math.floor(Math.random() * (max - min + 1) + min); // Logic to generate a random number between 1 and 5
 
 let selectedFortune;
 // Logic to select a fortune based on the random number
-if (randomNumber === 1) {
-  selectedFortune = fortune1;
-} else if (randomNumber === 2) {
-  selectedFortune = fortune2;
-} else if (randomNumber === 3) {
-  selectedFortune = fortune3;
-} else if (randomNumber === 4) {
-  selectedFortune = fortune4;
-} else if (randomNumber === 5) {
-  selectedFortune = fortune5;
-}
 
+switch (randomNumber) {
+  case 1:
+    selectedFortune = fortune1;
+    break;
+  case 2:
+    selectedFortune = fortune2;
+    break;
+  case 3:
+    selectedFortune = fortune3;
+    break;
+  case 4:
+    selectedFortune = fortune4;
+    break;
+  case 5:
+    selectedFortune = fortune5;
+    break;
+}
 console.log(selectedFortune);
