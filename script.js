65b204a0f130781a3aecc42c5c781cdf50e33410
Jester Rey Perez <165446384+jesterreyperez@users.noreply.github.com>
2026-09-21 13:42:51 +0800

    Put the logic inside a function

--- script.js
+++ script.js
@@ -9,26 +9,27 @@ const fortune5 = "Beware! A bug is hiding in your code.";
 const min = 1;
 const max = 5;
 
-const randomNumber = Math.floor(Math.random() * (max - min + 1) + min); // Logic to generate a random number between 1 and 5
-
-let selectedFortune;
-// Logic to select a fortune based on the random number
-
-switch (randomNumber) {
-  case 1:
-    selectedFortune = fortune1;
-    break;
-  case 2:
-    selectedFortune = fortune2;
-    break;
-  case 3:
-    selectedFortune = fortune3;
-    break;
-  case 4:
-    selectedFortune = fortune4;
-    break;
-  case 5:
-    selectedFortune = fortune5;
-    break;
+function getFortune() {
+  const randomNumber = Math.floor(Math.random() * (max - min + 1) + min); // Logic to generate a random number between 1 and 5
+  let selectedFortune;
+  // Logic to select a fortune based on the random number
+  switch (randomNumber) {
+    case 1:
+      selectedFortune = fortune1;
+      break;
+    case 2:
+      selectedFortune = fortune2;
+      break;
+    case 3:
+      selectedFortune = fortune3;
+      break;
+    case 4:
+      selectedFortune = fortune4;
+      break;
+    case 5:
+      selectedFortune = fortune5;
+      break;
+  }
+  return selectedFortune;
 }
-console.log(selectedFortune);
+console.log(getFortune());
